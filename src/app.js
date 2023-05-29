e5c8571b7b38e441294085e6e0aa52b407813569
Julian Juarez <julianjuarez98.jj@gmail.com>
2023-05-29 11:40:50 -0300

    se movio ProductManager a la carpeta src

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const ProductManager = require("../ProductManager.js");
+const ProductManager = require("./ProductManager.js");
 const productManager = new ProductManager();
 app.use(express.urlencoded({ extended: true }))
 
